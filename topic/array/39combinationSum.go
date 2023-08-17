d7fd3934f391b28296c735c1bed9ad1f05b36e10
longpi1 <longping1999@163.com>
2023-08-17 23:01:00 +0800

    add array algo

--- topic/array/39combinationSum.go
+++ topic/array/39combinationSum.go
@@ -1,5 +1,158 @@
 package main
 
+import "fmt"
+
+/*
+给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 target 的 所有 不同组合 ，并以列表形式返回。你可以按 任意顺序 返回这些组合。
+
+candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。
+
+对于给定的输入，保证和为 target 的不同组合数少于 150 个。
 
 
 
+示例 1：
+
+输入：candidates = [2,3,6,7], target = 7
+输出：[[2,2,3],[7]]
+解释：
+2 和 3 可以形成一组候选，2 + 2 + 3 = 7 。注意 2 可以使用多次。
+7 也是一个候选， 7 = 7 。
+仅有这两种组合。
+示例 2：
+
+输入: candidates = [2,3,5], target = 8
+输出: [[2,2,2,2],[2,3,3],[3,5]]
+示例 3：
+
+输入: candidates = [2], target = 1
+输出: []
+
+
+提示：
+
+1 <= candidates.length <= 30
+2 <= candidates[i] <= 40
+candidates 的所有元素 互不相同
+1 <= target <= 40
+
+解决思路：
+回溯算法：
+# 回溯算法
+
+回溯 <---->递归1.递归的下面就是回溯的过程
+
+2.回溯法是一个 纯暴力的 搜索
+
+3.回溯法解决的问题：
+
+	3.1组合 如：1234  两两组合
+
+	3.2切割问题 如：一个字符串有多少个切割方式 ，或者切割出来是回文
+
+	3.3子集 ： 1 2 3 4  的子集
+
+	3.4排列问题（顺序）
+
+	3.5棋盘问题：n皇后  解数独
+
+4.回溯可抽象成树形结构
+
+5.模板
+```go
+result = []
+func backtrack(选择列表,路径):
+    if 满足结束条件:
+        result.add(路径)
+        return
+    for 选择 in 选择列表:
+        做选择
+        backtrack(选择列表,路径)
+        撤销选择
+```
+*/
+
+//func combinationSum(candidates []int, target int) [][]int {
+//	var ans [][]int
+//	n := len(candidates)
+//	dfs := func(int) {}
+//	var result []int
+//	temp := target
+//	dfs = func(temp int) {
+//		if temp < 0 {
+//			return
+//		}
+//		if temp ==0 {
+//			tmp := make([]int, len(result))
+//			copy(tmp ,result)
+//			ans = append(ans, tmp)
+//			return
+//		}
+//		for i := 0; i < n ; i++ {
+//			if candidates[i] == 0 {
+//				continue
+//			}
+//			temp = temp - candidates[i]
+//
+//			result = append(result, candidates[i])
+//			dfs(temp)
+//			temp = temp + candidates[i]
+//			result = result[:n-1]
+//		}
+//	}
+//	dfs(temp)
+//
+//	return ans
+//}
+
+/*
+上述思路此外：原因如下：
+1.要避免重复寻找，for循环i应该通过变量传递，也就是startIndex,for i := 也就是startIndex; i < n ; i++ {错误；
+2.result = result[:n-1],应该为result = result[:len(result)-1] ，不然数组长度小于n时并不会成功回溯；
+
+树形结构应该如下：
+https://img-blog.csdnimg.cn/20201123202227835.png
+相关讲解视频：
+https://www.bilibili.com/video/BV1KT4y1M7HJ/?vd_source=34718180774b041b23050c8689cdbaf2
+*/
+
+func combinationSum(candidates []int, target int) [][]int {
+	var ans [][]int
+	n := len(candidates)
+	dfs := func(int,int) {}
+	var result []int
+	temp := target
+	dfs = func(temp int,startIndex int) {
+		if temp < 0 {
+			return
+		}
+		if temp ==0 {
+			tmp := make([]int, len(result))
+			copy(tmp ,result)
+			ans = append(ans, tmp)
+			return
+		}
+		for i := startIndex; i < n ; i++ {
+			if candidates[i] == 0 {
+				continue
+			}
+			temp = temp - candidates[i]
+
+			result = append(result, candidates[i])
+			dfs(temp,i)
+			temp = temp + candidates[i]
+			result = result[:len(result)-1]
+		}
+	}
+	dfs(temp,0)
+
+	return ans
+}
+
+
+
+func main()  {
+	candidates := []int{2,3,6,7}
+	target := 7
+	fmt.Printf("result:%v",combinationSum(candidates,target))
+}
---
+++ topic/array/40combinationSum2.go
@@ -0,0 +1,153 @@
+package main
+
+import "fmt"
+
+/*
+给定一个候选人编号的集合 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+
+candidates 中的每个数字在每个组合中只能使用 一次 。
+
+注意：解集不能包含重复的组合。
+
+
+
+示例 1:
+
+输入: candidates = [10,1,2,7,6,1,5], target = 8,
+输出:
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]
+示例 2:
+
+输入: candidates = [2,5,2,1,2], target = 5,
+输出:
+[
+[1,2,2],
+[5]
+]
+
+
+提示:
+
+1 <= candidates.length <= 100
+1 <= candidates[i] <= 50
+1 <= target <= 30
+
+解决思路：
+回溯算法：
+# 回溯算法
+
+回溯 <---->递归1.递归的下面就是回溯的过程
+
+2.回溯法是一个 纯暴力的 搜索
+
+3.回溯法解决的问题：
+
+	3.1组合 如：1234  两两组合
+
+	3.2切割问题 如：一个字符串有多少个切割方式 ，或者切割出来是回文
+
+	3.3子集 ： 1 2 3 4  的子集
+
+	3.4排列问题（顺序）
+
+	3.5棋盘问题：n皇后  解数独
+
+4.回溯可抽象成树形结构
+
+5.模板
+```go
+result = []
+func backtrack(选择列表,路径):
+    if 满足结束条件:
+        result.add(路径)
+        return
+    for 选择 in 选择列表:
+        做选择
+        backtrack(选择列表,路径)
+        撤销选择
+```
+*/
+
+
+
+//func combinationSum2(candidates []int, target int) [][]int {
+//	var ans [][]int
+//	var path []int
+//	n := len(candidates)
+//	used := make(map[int]bool, n)
+//	dfs := func(int) {}
+//	dfs = func(tmp int) {
+//		if tmp < 0 {
+//			return
+//		}
+//		if tmp == 0{
+//			var tmp []int
+//			copy(tmp, path)
+//			ans = append(ans, tmp)
+//		}
+//		for i := 0; i < n; i++ {
+//			if !used[i] {
+//				tmp = tmp - candidates[i]
+//				path = append(path, candidates[i])
+//				used[i] = true
+//				dfs(tmp)
+//				used[i] = false
+//				tmp = tmp + candidates[i]
+//				path = path[:len(path)-1]
+//			}
+//		}
+//	}
+//	dfs(target)
+//	return ans
+//}
+/*
+上述代码问题：
+	1.var tmp []int
+在创建要添加到ans数组的tmp副本时，赋值var tmp []int并随后的copy(tmp, path)是不正确的。你不能重新声明同名的变量，并且在调用copy之前，应该使用make创建一个新的切片。另外，tmp应该是一个局部变量。以下是修复后的代码：
+2.要避免重复寻找，for循环i应该通过变量传递，也就是startIndex,for i := 也就是startIndex; i < n ; i++ {错误；
+
+3.
+
+*/
+
+func combinationSum2(candidates []int, target int) [][]int {
+	var ans [][]int
+	var path []int
+	n := len(candidates)
+	used := make(map[int]bool, n)
+	dfs := func(int,int) {}
+	dfs = func(tmp int, index int) {
+		if tmp < 0 {
+			return
+		}
+		if tmp == 0{
+			tmp := make([]int, len(path))
+			copy(tmp, path)
+			ans = append(ans, tmp)
+		}
+		for i := index; i < n; i++ {
+			if !used[i] {
+				tmp = tmp - candidates[i]
+				path = append(path, candidates[i])
+				used[i] = true
+				dfs(tmp,i)
+				used[i] = false
+				tmp = tmp + candidates[i]
+				path = path[:len(path)-1]
+			}
+		}
+	}
+	dfs(target, 0)
+	return ans
+}
+
+func main()  {
+	candidates := []int{2,3,6,7}
+	target := 7
+	fmt.Printf("result:%v",combinationSum2(candidates,target))
+}
