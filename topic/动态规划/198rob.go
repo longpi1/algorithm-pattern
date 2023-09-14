a63d6ee67a145a4a2098cb52c536de01f4c8673b
longpi1 <longping1999@163.com>
2023-09-14 21:17:23 +0800

    update algo

--- topic/动态规划/198rob.go
+++ topic/动态规划/198rob.go
@@ -19,6 +19,67 @@ package main
 */
 
 
-func rob(nums []int) int {
+//func rob(nums []int) int {
+//	result := 0
+//	dp := make([]int, 0)
+//	n := len(nums)
+//	start := 0
+//	for  start < n{
+//		if nums[start+1] != nil {
+//			if nums[start+1]
+//		}else{
+//			result += nums[start]
+//		}
+//	}
+//}
 
+/*
+
+上述思路错误
+
+
+*/
+
+
+/*
+解题思路如下：
+确定dp数组（dp table）以及下标的含义
+dp[i]：考虑下标i（包括i）以内的房屋，最多可以偷窃的金额为dp[i]。
+确定递推公式
+决定dp[i]的因素就是第i房间偷还是不偷。
+如果偷第i房间，那么dp[i] = dp[i - 2] + nums[i] ，即：第i-1房一定是不考虑的，找出 下标i-2（包括i-2）以内的房屋，最多可以偷窃的金额为dp[i-2] 加上第i房间偷到的钱。
+如果不偷第i房间，那么dp[i] = dp[i - 1]，即考 虑i-1房，（注意这里是考虑，并不是一定要偷i-1房，这是很多同学容易混淆的点）
+然后dp[i]取最大值，即dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+
+*/
+func rob(nums []int) int {
+	n := len(nums)
+	dp := make([]int ,n)
+	//dp数组如何初始化
+	//从递推公式dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);可以看出，递推公式的基础就是dp[0] 和 dp[1]
+	//从dp[i]的定义上来讲，dp[0] 一定是 nums[0]，dp[1]就是nums[0]和nums[1]的最大值即：dp[1] = max(nums[0], nums[1]);
+	dp[0] = nums[0]
+	if n == 1 {
+		return nums[0]
+	}
+	dp[1] = max(nums[0],nums[1])
+
+	for i := 2; i < n; i++ {
+		//4.确定遍历顺序
+		//dp[i] 是根据dp[i - 2] 和 dp[i - 1] 推导出来的，那么一定是从前到后遍历！
+		dp[i] = max(dp[i-2]+nums[i],dp[i-1])
+	}
+	return dp[n-1]
+}
+
+func max(a int, b int) int {
+	if a >b {
+		return a
+	}
+	return b
+}
+
+func main(){
+	nums := []int{2,7,9,3,1}
+	print(rob(nums))
 }
---
+++ topic/动态规划/213rob2.go
@@ -0,0 +1,52 @@
+package main
+/*
+这道题目和198.打家劫舍是差不多的，唯一区别就是成环了。
+分两种情况考虑：
+1.考虑包含首元素，不包含尾元素
+2.考虑包含尾元素，不包含首元素
+《代码随想录》算法视频公开课：动态规划，房间连成环了那还偷不偷呢？| LeetCode：213.打家劫舍II，相信结合视频再看本篇题解，更有助于大家对本题的理解。
+
+作者：代码随想录
+链接：https://leetcode.cn/problems/house-robber-ii/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+
+func dfs(nums []int) int {
+	// 初始化两个变量，用于记录前两个房屋的最大抢劫价值
+	first, second := nums[0], max(nums[0], nums[1])
+
+	// 从第三个房屋开始遍历，依次计算每个房屋的最大抢劫价值
+	for _, v := range nums[2:] {
+		// 计算当前房屋的最大抢劫价值，同时更新first和second
+		first, second = second, max(first+v, second)
+	}
+
+	// 返回最终结果，即抢劫所有房屋的最大价值
+	return second
+}
+
+func rob(nums []int) int {
+	n := len(nums)
+
+	// 处理特殊情况，如果只有一个或两个房屋
+	if n == 1 {
+		return nums[0]
+	}
+	if n == 2 {
+		return max(nums[0], nums[1])
+	}
+
+	// 将问题分解成两个子问题，一个包括第一个房屋，另一个包括最后一个房屋
+	// 返回两个子问题的最大抢劫价值中的较大者
+	return max(dfs(nums[:n-1]), dfs(nums[1:]))
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+
